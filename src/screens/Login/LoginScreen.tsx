5a55262b7c850fa4ef5e12810dce3ebc11a7be09
João Paulo (Jp) <115497022+Joaolima2003@users.noreply.github.com>
2025-08-09 14:17:33 -0300

    Iniciando Projeto BemEstar+

---
+++ src/screens/Login/LoginScreen.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+export function LoginScreen() {
+  
+  
+  
+  
+  
+  
+ return (
+    <View style = {styles.centralizar}>
+        
+
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  centralizar: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
