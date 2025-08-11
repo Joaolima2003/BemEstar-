9916445bd721e2fb5392f2f12c92ccf7c313ceeb
João Paulo (Jp) <115497022+Joaolima2003@users.noreply.github.com>
2025-08-10 21:38:43 -0300

    enviando login

--- src/screens/Login/LoginScreen.tsx
+++ src/screens/Login/LoginScreen.tsx
@@ -1,16 +1,51 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, TouchableOpacity} from 'react-native';
 
 export function LoginScreen() {
+  const [user, setUser] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const [hidePassword, setHidePassword] = React.useState(true)
   
-  
-  
+  function handleLogin(){
+    console.log('User', user)
+    console.log('Password', password)
+
+    setUser('')
+    setPassword('')
+  }
   
   
   
  return (
-    <View style = {styles.centralizar}>
-        
+    <View style = {styles.centralize}>
+        <View style = {styles.box}>
+            <View style = {styles.titleLogin}>
+                <Text>Login</Text>
+
+            </View>
+            <View style = {styles.inputs}>
+                <View style = {styles.input}>
+                    <TextInput
+                        placeholder='User'
+                        value={user}
+                        onChangeText={setUser}
+                    />
+                </View>
+                <View style = {styles.input}>
+                    <TextInput
+                        placeholder='Password'
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry={true}
+                    />
+                    <TouchableOpacity
+                        
+                    
+                    />
+                </View>
+                <Button title='Join' onPress={handleLogin}></Button>
+            </View>
+        </View>
 
     </View>
   );
@@ -18,10 +53,35 @@ export function LoginScreen() {
 
 
 const styles = StyleSheet.create({
-  centralizar: {
+  centralize: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#dedede',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box: {
+    borderWidth: 1,
+    width: '80%',
+    height: 300,
+    padding: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    elevation: 5,
+  },
+  titleLogin: {
+    marginBottom: 20,
+    alignItems: 'center',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  inputs: {
+    gap: 10
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+  }
 });
